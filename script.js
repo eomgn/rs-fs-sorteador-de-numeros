8bcda73a0fef9a40b95750317b10d66f4a7fe238
eomgn <matheeusgn21@gmail.com>
2025-05-08 20:06:41 -0300

    adding comments

--- script.js
+++ script.js
@@ -20,9 +20,10 @@ const listingNumbers = document.querySelector(".listingNumbers");
 // criando elemento de ul
 const ul = document.createElement("ul");
 
+// array que vai receber os números sorteados
 let listNumbers = [];
 
-// executando evento de click ao clicar em sortear
+// monitora ação de click no input submit e executa a função passada
 submitSortition.addEventListener("click", (event) => {
   event.preventDefault();
 
@@ -31,82 +32,91 @@ submitSortition.addEventListener("click", (event) => {
   intervalInput.value = "";
   startInput.value = "";
   endInput.value = "";
-
-  console.log(listNumbers);
 });
 
-submitNewSortition.addEventListener("click", () => {
-  newSortition();
-});
+// monitora ação de click no input submit e executa a função passada
+submitNewSortition.addEventListener("click", () => newSortition());
 
-// gerando pelo intervalo - SEM possibilidade de repeticao
+// gerar números aleatórios sem repetição dentro do intervalo definido
 function getNumbersNoRepeat(interval, start, end) {
+  // verifica se o número de sorteios ultrapassa a quantidade de valores únicos possíveis
   if (interval > end - start + 1) {
     alert("O intervalo excede a quantidade de valores únicos disponíveis.");
     return;
   }
 
+  // gera números aleatórios únicos até atingir a quantidade desejada no parametro interval
   for (let i = 0; i < interval; i++) {
     let value = getRandomNumber(start, end);
     let attempts = 0;
 
+    // caso o número gerado já existir na lista, gera um novo número (até 100 tentativas)
     while (listNumbers.includes(value)) {
       if (attempts > 100) {
         console.log("Sorteio concluído!!!");
         return;
       }
 
+      // gera um NOVO número caso já existe no array
       value = getRandomNumber(start, end);
+
+      // incrementa em attempts em cada iteração
       attempts++;
     }
 
+    // adiciona o número à lista final
     listNumbers.push(value);
   }
 
+  // chama a função para exibir os resultados na tela
   createList(listNumbers);
 }
 
-// gerando pelo intervalo - possibilidade de repeticao
+// gerar números aleatórios com possibilidade de repetição
 function getNumbersWithRepeat(interval, start, end) {
+  // gera números aleatórios únicos até atingir a quantidade desejada no parametro interval
   for (let i = 0; i < interval; i++) {
     let value = getRandomNumber(start, end);
     listNumbers.push(value);
   }
 
+  // chama a função para exibir os resultados na tela
   createList(listNumbers);
 }
 
+// função chamada ao clicar em "Sortear novamente", que redefine os textos e exibe novamente o formulário
 function newSortition() {
+  // ajustar o alinhamento e redefinir o título e descrição do cabeçalho
   sortitionHeader.style.textAlign = "start";
-
   sortitionHeaderTitle.textContent = "quero sortear:";
-
   sortitionHeaderP.textContent = `Defina o intervalo e a quantidade de números, clique em "Sortear" e veja os resultados na tela. É rápido e fácil!`;
   sortitionHeaderP.style.fontWeight = "normal";
   sortitionHeaderP.style.textTransform = "initial";
 
+  // oculta a área com a lista de números e exibe o formulário novamente
   hide(listingNumbers);
   appear(form);
 }
 
+// função responsável por criar a lista (ul) de números sorteados e exibir no HTML
 function createList(arr) {
-  ul.innerHTML = "";
-  listNumbers = [];
+  ul.innerHTML = ""; //limpa a lista atual
+  listNumbers = []; // reinicia o array de números
 
-  // aplicando ao form a funcao para esconder (display: none)
+  // aplicando a funcao para ocultar o form (display: none)
   hide(form);
 
   // input submit apos criação da lista para aplicar class .show para animacao
-  const newSortition = document.querySelector(".newSortition");
+  const newSortition = document.querySelector(".newSortition"); // Botão de novo sorteio
 
+  // atualizar o cabeçalho para indicar que o resultado foi gerado
   sortitionHeader.style.textAlign = "center";
-
   sortitionHeaderTitle.textContent = "Resultado do sorteio";
-
   sortitionHeaderP.textContent = "1º resultado";
   sortitionHeaderP.style.fontWeight = "800";
   sortitionHeaderP.style.textTransform = "uppercase";
 
+  // para cada número no array, cria um <li>, e adiciona à lista e aplica animação
   for (const i of arr) {
     const li = document.createElement("li");
     li.textContent = i;
@@ -115,82 +125,97 @@ function createList(arr) {
 
     li.classList.add("animate");
   }
+
+  // adicionando a lista (ul) ao HTML
   listingNumbers.prepend(ul);
 
+  // aplicando a animação no botão de novo sorteio
   if (newSortition) {
     setTimeout(() => {
       newSortition.classList.add("show");
-    }, 200); // pequeno atraso para suavidade extra
+    }, 200);
   }
 
+  // exibe a área que comtem a lista com os resultados no HTML
   appear(listingNumbers);
 }
 
-// verificar inputs
+// função responsável por validar os campos do formulário antes de executar o sorteio
 function verifyInputs(interval, start, end) {
+  // convertendo os valores recebidos em números inteiros
   interval = parseInt(interval);
   start = parseInt(start);
   end = parseInt(end);
 
   // console.log(interval)
 
+  // valida se os valores de start e end são válidos, ou seja, se são números
   if (isNaN(start) || isNaN(end)) {
     alert(
       "Preencha o número inicial (De) e o número final (Até) para o sorteio."
     );
     return;
   }
-
+  // valida se o interval é válido, ou seja, se é número
   if (isNaN(interval)) {
     alert("Preencha a quantidade de Números para serem sorteados.");
     return;
   }
 
+  // impede mais de 20 números sendo sorteados
   if (interval > 20) {
     alert("A quantidade de Números sorteados deve ser menor que 20.");
     return;
   }
 
+  // impede que o valor inicial seja igual ao final
   if (start == end) {
     alert("O número inicial (De) NÃO ser igual ao final (Até).");
     return;
   }
 
+  // impede que o valor inicial seja maior que o final
   if (start > end) {
     alert("O número inicial (De) NÃO pode ser maior que final (Até).");
     return;
   }
 
+  // executa o sorteio conforme a opção do switch: com ou sem repetição
   if (switchInput.checked) {
+    // sem repetição
     getNumbersNoRepeat(interval, start, end);
   } else {
+    // com repetição
     getNumbersWithRepeat(interval, start, end);
   }
 }
 
+// função auxiliar que oculta um elemento HTML (display: none)
 function hide(element) {
   element.style.display = "none";
 }
 
+// função auxiliar que exibe um elemento HTML (display: block)
 function appear(element) {
   element.style.display = "block";
 }
 
-// gerando numero aleatório
+// função que retorna um número inteiro aleatório
 function getRandomNumber(start, end) {
-  start = Math.ceil(start);
-  end = Math.floor(end);
+  start = Math.ceil(start); // Arredonda start para cima
+  end = Math.floor(end); // Arredonda end para baixo
   return Math.floor(Math.random() * (end - start + 1) + start);
 }
 
-// garantindo numeros inteiros positivos
+// garante que os inputs do formulário aceitem apenas números inteiros positivos
 watchInput(intervalInput);
 watchInput(startInput);
 watchInput(endInput);
 
+// função que monitora a entrada de dados em um input e remove qualquer caractere não numérico
 function watchInput(input) {
   input.addEventListener("input", () => {
     const value = input.value;
-    input.value = value.replace(/\D+/g, "");
+    input.value = value.replace(/\D+/g, ""); // Remove tudo que não for número
   });
 }
